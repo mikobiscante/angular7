6e8c795dcb406f742fc4a4326ed8b4a854870d3a
Michael Biscante <michael.biscante@gmail.com>
2019-10-06 19:56:44 +0800

    angular7(UI): added back to top button using jquery and added font-family Montserrat

--- src/app/post/view-post/view-post.component.ts
+++ src/app/post/view-post/view-post.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PostService } from '../../_shared/services/post.service';
 import { ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
 import { Post } from '../../_shared/models/post';
+declare var $: any;
 
 @Component({
   selector: 'app-view-post',
   templateUrl: './view-post.component.html',
   styleUrls: ['./view-post.component.scss']
 })
-export class ViewPostComponent implements OnInit {
+export class ViewPostComponent implements OnInit, AfterViewInit {
   post: Post;
   private postId: number | string;
 
@@ -22,6 +23,27 @@ export class ViewPostComponent implements OnInit {
 
     this.getPostById(this.postId);
   }
+
+  ngAfterViewInit() {
+    // On scrollTop over 100, show back to top button
+    $(window).scroll(function () {
+      const btn = $('.back-to-top');
+      const scrollTop = $(window).scrollTop();
+      if (scrollTop > 100) {
+        btn.addClass('d-flex');
+      } else {
+        btn.removeClass('d-flex');
+      }
+    });
+
+  }
+
+  // On click button backt top top, scrollTop to 0
+  backToTop(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  }
+
 
   getPostById(id) {
     this.postService.get(id).pipe(first()).subscribe(data => {
